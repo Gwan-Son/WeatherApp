497524edb1ad72e5eb0c9e676e1f7c63bc3706aa
GwanSon <id147369@naver.com>
2024-08-23 12:33:15 +0900

    Initial commit

---
+++ my-weather/my_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_weatherApp.swift
+//  my-weather
+//
+//  Created by 심관혁 on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct my_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my-weatherUITests/my_weatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  my_weatherUITestsLaunchTests.swift
+//  my-weatherUITests
+//
+//  Created by 심관혁 on 8/23/24.
+//
+
+import XCTest
+
+final class my_weatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
