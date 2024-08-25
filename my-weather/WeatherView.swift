b554cb0200593e61714c8ab508de58d097971194
GwanSon <id147369@naver.com>
2024-08-25 16:50:37 +0900

    feat: ContentView rename
    ContentView를 WeatherView로 이름 변경 후 viewModel에서 정보를 가져오는 코드 추가

---
+++ my-weather/WeatherView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  my-weather
+//
+//  Created by 심관혁 on 8/23/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    @ObservedObject var viewModel = WeatherViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("날씨 정보")
+                .font(.largeTitle)
+                .padding()
+            
+            Text(viewModel.temperature)
+                .font(.title)
+                .padding()
+            
+            Text(viewModel.description)
+                .font(.subheadline)
+                .padding()
+            
+            Button {
+                viewModel.fetchWeather(nx: "60", ny: "127")
+            } label: {
+                Text("날씨 가져오기")
+                    .font(.title2)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WeatherView()
+}
