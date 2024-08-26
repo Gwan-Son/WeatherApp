874d450a0bfc28751736068d690d72b1689fa5d2
GwanSon <id147369@naver.com>
2024-08-26 17:16:46 +0900

    feat: 단기예보 JSON 불러오기
    공공데이터 API를 사용해서 단기예보 JSON을 불러오는 코드 작성

--- my-weather/WeatherService.swift
+++ my-weather/WeatherService.swift
@@ -8,29 +8,31 @@
 import Alamofire
 
 class WeatherService {
-    let apiKey = "ZLXSIs%2F6T3udLlgXrsOuMCfzqlqzkVMXnahTmkqvxPbjuT0HwGt7HcSGY8JCdI%2F4vpkpu6RhdUHnRuwNWusjiA%3D%3D"
+    let apiKey = Bundle.main.infoDictionary?["APIKey"] as! String
     
     func getWeather(for nx: String, ny: String, completion: @escaping (WeatherResponse?) -> Void) {
-        let url = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+        let url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
         
-        let params = [
+        let params: [String: String] = [
             "serviceKey": apiKey,
+            "numOfRows": "60",
             "pageNo": "1",
-            "numOfRows": "1000",
             "dataType": "JSON",
             "base_date": getCurrentDate(),
-            "base_time": "0500",
+            "base_time": "0500", // TODO: - 현재 시각별로 계산하는 dateFomatter 추가해야함
             "nx": nx,
             "ny": ny
         ]
         
-        AF.request(url, parameters: params).responseDecodable(of: WeatherResponse.self) { response in
+        AF.request(url, method: .get, parameters: params as Parameters, encoding: URLEncoding.default)
+            .responseDecodable(of: WeatherResponse.self) { response in
             switch response.result {
             case .success(let response):
-                completion(response)
+                print(response.response.body.items.item)
+//                completion(value)
             case .failure(let error):
-                print("Error: \(error.localizedDescription)")
-                completion(nil)
+                print(error)
+//                print("Error: \(error.localizedDescription)")
             }
         }
     }
