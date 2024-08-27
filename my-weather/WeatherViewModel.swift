23ca097bf2c20bff0cd1ec71cbb3f6ccdaef3a36
GwanSon <id147369@naver.com>
2024-08-27 18:22:15 +0900

    feat: ViewModel 기능 수정
    WeatherResponse의 category값에 따라 View로 데이터 전달 및 추후에 하늘상태에 사용할 함수 생성

--- my-weather/WeatherViewModel.swift
+++ my-weather/WeatherViewModel.swift
@@ -18,13 +18,13 @@ class WeatherViewModel: ObservableObject {
             guard let self = self else { return }
             guard let items = weatherResponse?.response.body.items.item else { return }
             
-//            for item in items {
-//                if item.category.rawValue == "T1H" {
-//                    self.temperature = "\(item.fcstValue)°C"
-//                } else if item.category.rawValue == "PTY" {
-//                    self.description = self.parsePTY(value: item.fcstValue)
-//                }
-//            }
+            for item in items {
+                if item.category == "TMP" {
+                    self.temperature = "\(item.fcstValue)°C"
+                } else if item.category == "PTY" {
+                    self.description = self.parsePTY(value: item.fcstValue)
+                }
+            }
             print("완료")
         }
     }
@@ -39,6 +39,21 @@ class WeatherViewModel: ObservableObject {
             return "비/눈"
         case "3":
             return "눈"
+        case "4":
+            return "소나기"
+        default:
+            return "알 수 없음"
+        }
+    }
+    
+    private func parseSKY(value: String) -> String {
+        switch value {
+        case "1":
+            return "맑음"
+        case "3":
+            return "구름많음"
+        case "4":
+            return "흐림"
         default:
             return "알 수 없음"
         }
