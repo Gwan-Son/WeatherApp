f10e1730c0c1776287d760b23b9ad7a2341b5d81
GwanSon <id147369@naver.com>
2024-08-28 18:54:24 +0900

    feat: WeatherView 폴더 이동

--- my-weather/WeatherView/WeatherView.swift
+++ my-weather/WeatherView/WeatherView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct WeatherView: View {
     @ObservedObject var viewModel = WeatherViewModel()
     
+    
     var body: some View {
         VStack {
             Text("날씨 정보")
@@ -25,7 +26,7 @@ struct WeatherView: View {
                 .padding()
             
             Button {
-                viewModel.fetchWeather(nx: "60", ny: "127")
+                viewModel.fetchWeather(nx: "37", ny: "127")
             } label: {
                 Text("날씨 가져오기")
                     .font(.title2)
