6ebd7d6f47a9cc2bdf2cc74efe224111b767130e
GwanSon <id147369@naver.com>
2024-08-30 17:10:22 +0900

    feat: WeatherView 디자인 수정

--- my-weather/WeatherView/WeatherView.swift
+++ my-weather/WeatherView/WeatherView.swift
@@ -13,32 +13,24 @@ struct WeatherView: View {
     
     
     var body: some View {
-        VStack {
-            Text("날씨 정보")
-                .font(.largeTitle)
-                .padding()
-            
-            Text(viewModel.temperature)
-                .font(.title)
-                .padding()
-            
-            Text(viewModel.description)
-                .font(.subheadline)
-                .padding()
-            
-            Button {
-                viewModel.fetchWeather(nx: "37", ny: "127")
-            } label: {
-                Text("날씨 가져오기")
-                    .font(.title2)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+        ZStack {
+            BackgroundView(topColor: .blue, bottomColor: .purple)
+            VStack {
+                CityTextView(cityName: "서울특별시 도봉구")
+                WeatherStatusView(imageName: viewModel.description, temperature: viewModel.temperature)
+                
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 38)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 42)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "cloud.sun.bolt.fill", temperature: 32)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.rain.fill", temperature: 30)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.hail.fill", temperature: 28)
+                }
+                
+                Spacer()
             }
-
         }
-        .padding()
         .task {
             await locationManager.checkUserDeviceLocationServiceAuthorization()
         }
@@ -47,4 +39,69 @@ struct WeatherView: View {
 
 #Preview {
     WeatherView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct WeatherStatusView: View {
+    var imageName: String
+    var temperature: String
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text(temperature)
+                .font(.system(size: 70, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
 }
